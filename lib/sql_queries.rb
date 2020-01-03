23d97b8d537fc9d9b7f16472229168dcee7ff994
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-03 01:01:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "SELECT p.title, pl.amount FROM projects p JOIN pledges pl ON p.id = pl.project_id WHERE p.category = 'music'"
+  "SELECT p.category, pl.amount FROM projects p JOIN pledges pl ON p.id = pl.project_id WHERE p.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
