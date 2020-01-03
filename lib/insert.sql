15f23d50a7834637396837456696398f4116bf97
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-03 00:01:30 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,5 @@
 --Within the insert.sql file, insert 10 projects, 20 users, and 30 pledges into the database.
+-- projects: title, category, funding_goal, start_date, end_date
+-- users: name, age
+-- pledge: amount, user_id, project_id
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Kitten Mittens', 'Pets', 10000, 01-01-2020);
